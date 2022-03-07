267ab005309418c6ad874cc0b593153e656608c4
MateusP21 <mateuspasso@outlook.com>
2022-03-07 17:18:36 -0300

    pesquisa feita

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,14 +2,52 @@ import React from 'react';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <div data-testid="page-search">
-          <h1>Search</h1>
-        </div>
-      </>
-    );
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      isDisabled: true,
+    };
   }
+
+    handleSearch = (e) => {
+      const maxLength = 2;
+      const searchText = e.target.value;
+      this.setState({
+        search: searchText,
+      });
+      if (searchText.length >= maxLength) {
+        this.setState({ isDisabled: false });
+      } else {
+        this.setState({ isDisabled: true });
+      }
+    }
+
+    render() {
+      const { isDisabled, search } = this.state;
+      return (
+        <>
+          <Header />
+          <div data-testid="page-search">
+            <form>
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                placeholder="Nome do Artista"
+                onChange={ this.handleSearch }
+                value={ search }
+              />
+              <button
+                disabled={ isDisabled }
+                data-testid="search-artist-button"
+                type="button"
+              >
+                Procurar
+              </button>
+            </form>
+          </div>
+        </>
+      );
+    }
 }
